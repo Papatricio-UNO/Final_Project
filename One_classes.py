8b0d10f60a2d3f15c2c6a842c351617fb02ad31d
Papatricio-UNO <patparonable@gmail.com>
2022-05-02 18:01:05 -0500

    Added two buttons each for power and mute function. The buttons are to turn each function on or off. Also, altered the coding for the volume and channel functions.

--- One_classes.py
+++ One_classes.py
@@ -17,8 +17,9 @@ class Television(QMainWindow, Ui_ProjectOne_MainWindow):
         self.__status = False
         self.__mute = False
 
-        # Power button
-        self.power_button.clicked.connect(self.power)
+        # Power buttons
+        self.PowerOn_btn.clicked.connect(self.power_on)
+        self.PowerOff_btn.clicked.connect(self.power_off)
 
         # Channel buttons
         self.ChannelDown_btn.clicked.connect(self.channel_down)
@@ -28,56 +29,67 @@ class Television(QMainWindow, Ui_ProjectOne_MainWindow):
         self.VoluemDown_btn.clicked.connect(self.volume_down)
         self.VolumeUp_btn.clicked.connect(self.volume_up)
 
-        # Mute button
-        self.Mute_btn.clicked.connect(self.mute)
+        # Mute buttons
+        self.MuteOn_btn.clicked.connect(self.mute_on)
+        self.MuteOff_btn.clicked.connect(self.mute_off)
 
-    def power(self):
-        if self.power_button.isChecked():
-            if not self.__status:
-                self.__status = True
-                self.power_label.setText('On')
-            else:
-                self.__status = False
-                self.power_label.setText('Off')
+    def power_on(self):
+        self.__status = True
+        self.PowerOn_label.setText(f'Power: {self.__status}')
+        self.Channels_label.setText(f'Channel: {self.__channel}')
+        self.Volume_label.setText(f'Volume: {self.__volume}')
+        self.Mute_label.setText(f'Mute: {self.__mute}')
 
-    def mute(self):
-        if self.Mute_btn.isChecked():
+    def power_off(self):
+        self.__status = False
+        self.__mute = False
+        self.__channel = Television.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.PowerOn_label.setText(f'Power: ')
+        self.Channels_label.setText(f'Channel: ')
+        self.Volume_label.setText(f'Volume: ')
+        self.Mute_label.setText(f'Mute: ')
+
+    def mute_on(self):
+        if self.__status:
             self.__mute = True
-        else:
+            self.Mute_label.setText(f'Mute: {self.__mute}')
+
+    def mute_off(self):
+        if self.__status:
             self.__mute = False
+            self.Mute_label.setText(f'Mute: {self.__mute}')
 
     def channel_up(self):
-        if self.ChannelUp_btn.isChecked():
-            if self.__status:
-                if self.__channel < Television.MAX_CHANNEL:
-                    self.__channel += 1
-                    self.Channels_label.setText(f'Channel: {self.__channel}')
-                else:
-                    self.__channel = Television.MIN_CHANNEL
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+                self.Channels_label.setText(f'Channel: {self.__channel}')
+            else:
+                self.__channel = Television.MIN_CHANNEL
+                self.Channels_label.setText(f'Channel: {self.__channel}')
 
     def channel_down(self):
-        if self.ChannelDown_btn.isChecked():
-            if self.__status:
-                if self.__channel > Television.MIN_CHANNEL:
-                    self.__channel -= 1
-                    self.Channels_label.setText(f'Channel: {self.__channel}')
-                else:
-                    self.__channel = Television.MAX_CHANNEL
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+                self.Channels_label.setText(f'Channel: {self.__channel}')
+            else:
+                self.__channel = Television.MAX_CHANNEL
+                self.Channels_label.setText(f'Channel: {self.__channel}')
 
     def volume_up(self):
-        if self.VolumeUp_btn.isChecked():
-            if self.__status:
-                if self.__mute and self.__volume != Television.MAX_VOLUME:
-                    self.volume += 1
-                    self.Volume_label.sextText(f'Volume: {self.__volume}')
+        if self.__status:
+            if not self.__mute and self.__volume != Television.MAX_VOLUME:
+                self.__volume += 1
+                self.Volume_label.setText(f'Volume: {self.__volume}')
 
     def volume_down(self):
-        if self.VoluemDown_btn.isChecked():
-            if self.__status:
-                if self.__mute and self.__volume != Television.MIN_VOLUME:
-                    self.volume -= 1
-                    self.Volume_label.sextText(f'Volume: {self.__volume}')
+        if self.__status:
+            if not self.__mute and self.__volume != Television.MIN_VOLUME:
+                self.__volume -= 1
+                self.Volume_label.setText(f'Volume: {self.__volume}')
 
-#     def __str__(self):
-#          return f'TV status: Is on = {self.__status}, Mute status: Is on = {self.__mute}'\
-#                 f'Channel = {self.__channel}, Volume = {self.__volume}'
+    def __str__(self):
+        return f'TV status: Is on = {self.__status}, Mute status: Is on = {self.__mute},' \
+               f'Channel = {self.__channel}, Volume = {self.__volume}'
