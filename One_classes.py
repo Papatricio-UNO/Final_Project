bf85cd51014049cd0d784e8d8e495690d645256f
Papatricio-UNO <patparonable@gmail.com>
2022-05-02 10:06:13 -0500

    Modified One_classes.py and One_main.py, so the program can run a functional remote on GUI. The coding is still incomplete.

--- One_classes.py
+++ One_classes.py
@@ -1,52 +1,63 @@
-class Television:
-    MIN_CHANNEL = 0     # Minimum TV channel
-    MAX_CHANNEL = 3     # Maximum TV channel
+from PyQt5.QtWidgets import *
+from One_view import Ui_ProjectOne_MainWindow
 
-    MIN_VOLUME = 0      # Minimum TV volume
-    MAX_VOLUME = 2      # Maximum TV volume
+class Television(QMainWindow, Ui_ProjectOne_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().init(*args, **kwargs)
+        self.setupUi()
 
-    def __init__(self):
-        self.__channel = Television.MIN_CHANNEL
-        self.__volume = Television.MIN_VOLUME
-        self.__status = False
-        self.__mute = False
+        self
 
-    def power(self):
-        if not self.__status:
-            self.__status = True
-        else:
-            self.__status = False
 
-    def mute(self):
-        if not self.__nute:
-            self.__mute = True
-        else:
-            self.__mute = False
-
-    def channel_up(self):
-        if self.__status:
-            if self.__channel < Television.MAX_CHANNEL:
-                self.__channel += 1
-            else:
-                self.__channel = Television.MIN_CHANNEL
-
-    def channel_down(self):
-        if self.__status:
-            if self.__channel > Television.MIN_CHANNEL:
-                self.__channel -= 1
-            else:
-                self.__channel = Television.MAX_CHANNEL
-
-    def volume_up(self):
-        if self.__mute:
-            if self.__status and self.__volume != Television.MAX_VOLUME:
-                self.__volume += 1
-
-    def volume_down(self):
-        if self.__mute:
-            if self.__status and self.__volume != Television.MIN_VOLUME:
-                self.__volume -= 1
-
-    def __str__(self):
-        return f'TV status: Is on = {self.__status}, ' \
-               f'Channel = {self.__channel}, Volume = {self.__volume}'
+# class Television:
+#     MIN_CHANNEL = 0  # Minimum TV channel
+#     MAX_CHANNEL = 3  # Maximum TV channel
+#
+#     MIN_VOLUME = 0  # Minimum TV volume
+#     MAX_VOLUME = 2  # Maximum TV volume
+#
+#     def __init__(self):
+#         self.__channel = Television.MIN_CHANNEL
+#         self.__volume = Television.MIN_VOLUME
+#         self.__status = False
+#         self.__mute = False
+#
+#     def power(self):
+#         if not self.__status:
+#             self.__status = True
+#         else:
+#             self.__status = False
+#
+#     def mute(self):
+#         if not self.__mute:
+#             self.__mute = True
+#         else:
+#             self.__mute = False
+#
+#     def channel_up(self):
+#         if self.__status:
+#             if self.__channel < Television.MAX_CHANNEL:
+#                 self.__channel += 1
+#             else:
+#                 self.__channel = Television.MIN_CHANNEL
+#
+#     def channel_down(self):
+#         if self.__status:
+#             if self.__channel > Television.MIN_CHANNEL:
+#                 self.__channel -= 1
+#             else:
+#                 self.__channel = Television.MAX_CHANNEL
+#
+#     def volume_up(self):
+#         if self.__mute:
+#             if self.__status and self.__volume != Television.MAX_VOLUME:
+#                 self.__volume += 1
+#
+#     def volume_down(self):
+#         if self.__mute:
+#             if self.__status and self.__volume != Television.MIN_VOLUME:
+#                 self.__volume -= 1
+#
+#     def __str__(self):
+#         return f'TV status: Is on = {self.__status}, ' \
+#                f'Channel = {self.__channel}, Volume = {self.__volume}'
--- One_main.py
+++ One_main.py
@@ -1,15 +1,16 @@
 # Project One: Updated Lab 9
 # Features: GUI, mute button
 
-from One_classes import *       # import statement needed to gain access to Television class
-from One_view import *
+from One_classes import *
 
 
 def main():
-    pass
-
+    app = QApplication([])
+    window = Television()
+    window.show()
+    window.setFixedSize(250, 300)
+    app.exec_()
 
 
 if __name__ == '__main__':
     main()
-
