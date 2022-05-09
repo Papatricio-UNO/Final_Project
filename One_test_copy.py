15a50896d355d2cb3964ebc6613d13df79ec520c
Papatricio-UNO <patparonable@gmail.com>
2022-05-09 12:24:08 -0500

    Changed coding to adapt to Television.py changes.

--- One_test_copy.py
+++ One_test_copy.py
@@ -12,17 +12,18 @@ class MyTestCase(unittest.TestCase):
         del self.tv1
 
     def test_init(self):
-        self.tv1.power_on()
+        self.tv1.power()
         self.assertEqual(self.tv1.__str__(), 'TV status: Is on = True, Mute status: Is on = False,' 'Channel = 0, '
                                              'Volume = 0')
 
     def test_power_off(self):
-        self.tv1.power_on()
+        self.tv1.power()
         self.assertEqual(self.tv1.__str__(), 'TV status: Is on = True, Mute status: Is on = False,' 'Channel = 0, '
                                              'Volume = 0')
-        self.tv1.power_off()
+        self.tv1.power()
         self.assertEqual(self.tv1.__str__(), 'TV status: Is on = False, Mute status: Is on = False,' 'Channel = 0, '
                                              'Volume = 0')
+
 
 if __name__ == '__main__':
     unittest.main()
