cb1261d49fb81b63c02a885bf8b3a4bd147fae20
Papatricio-UNO <patparonable@gmail.com>
2022-05-09 11:29:02 -0500

    Added One_main_copy.py for copy files

---
+++ One_main_copy.py
@@ -0,0 +1,16 @@
+# Project One: Updated Lab 9
+# Added Features: GUI, mute button, on / off buttons for power and mute functions
+
+from One_classes_copy import *
+
+
+def main():
+    app = QApplication([])
+    window = GUI()
+    window.show()
+    window.setFixedSize(290, 295)
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
