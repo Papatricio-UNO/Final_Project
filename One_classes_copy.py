0b3cfe569bad6cddaec48d4a566192ce97dd40a9
Papatricio-UNO <patparonable@gmail.com>
2022-05-09 12:28:09 -0500

    Added comment on what to do next time

--- One_classes_copy.py
+++ One_classes_copy.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import *
 from One_view import Ui_ProjectOne_MainWindow
 from One_Television import *
 
-# To Do Make new GUI file with one power btn and mute btn and updated UI with own TV SCREEN
+# To Do: Make new GUI file with one power btn and mute btn and updated UI with own TV SCREEN
 
 class GUI(QMainWindow, Ui_ProjectOne_MainWindow):
     def __init__(self, *args, **kwargs) -> None:
@@ -32,15 +32,15 @@ class GUI(QMainWindow, Ui_ProjectOne_MainWindow):
             self.tv.volume()
             self.TV_screen.setText(f'TV is on: {self.tv.status}')
 
-            def gui_channel_up(self) -> None:
-                self.tv.channel_up()
-                self.TV_screen.setText(f'TV status, TV volume, TV channel')
+        def gui_channel_up(self) -> None:
+            self.tv.channel_up()
+            self.TV_screen.setText(f'TV status, TV volume, TV channel')
 
-                def gui_channel_down(self) -> None:
-                    pass
+        def gui_channel_down(self) -> None:
+            pass
 
-                def gui_volume_up(self) -> None:
-                    pass
+        def gui_volume_up(self) -> None:
+            pass
 
-                def gui_volume_down(self) -> None:
-                    pass
+        def gui_volume_down(self) -> None:
+            pass
